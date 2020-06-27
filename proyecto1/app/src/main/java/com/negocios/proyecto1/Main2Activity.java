74eed25f3cd5b029d1ee1723aa2656fe31a693ac
Leonardo Alvarado <10leoalvarado@gmail.com>
2020-06-27 01:13:36 -0600

    Fernando y Julio

--- proyecto1/app/src/main/java/com/negocios/proyecto1/Main2Activity.java
+++ proyecto1/app/src/main/java/com/negocios/proyecto1/Main2Activity.java
@@ -3,15 +3,42 @@ package com.negocios.proyecto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class Main2Activity extends AppCompatActivity {
+    EditText ListaItems;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private JSONArray listJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ListaItems = findViewById(R.id.ListaItems);
+
+        this.preferences = getSharedPreferences("Immuni", MODE_PRIVATE);
+
+        try {
+            listJson = new JSONArray(this.preferences.getString("data", "[]"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        for (int i=0; i<listJson.length(); i++){
+            try {
+                ListaItems.append("Nombre: " + listJson.getJSONObject(i).getString("Nombre").toString()+ "\nEdad:  " + listJson.getJSONObject(i).getString("Edad").toString()+"\n\t -----------\n");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public  void atras(View view){
--- proyecto1/app/src/main/java/com/negocios/proyecto1/MainActivity.java
+++ proyecto1/app/src/main/java/com/negocios/proyecto1/MainActivity.java
@@ -3,18 +3,65 @@ package com.negocios.proyecto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ptnombre;
+    EditText ptedad;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    private SharedPreferences preference;
+    private SharedPreferences.Editor editor;
+    private JSONArray ImmuniD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ptnombre = findViewById(R.id.ptnombre);
+        ptedad = findViewById(R.id.ptedad);
+
+        preference = getSharedPreferences("Immuni", MODE_PRIVATE);
+        editor = preference.edit();
+
+        try {
+            ImmuniD = new JSONArray(preference.getString("data", "[]"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public void enviar(View view){
+        String Nombre = ptnombre.getText().toString();
+        String Edad = ptedad.getText().toString();
+
+        JSONObject ImmuniItems = new JSONObject();
+        try {
+            ImmuniItems.put("Nombre", Nombre);
+            ImmuniItems.put("Edad", Edad);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ImmuniD.put(ImmuniItems);
+
+        editor.putString("data", ImmuniD.toString());
+        editor.commit();
+
+        ptnombre.setText("");
+        ptedad.setText("");
+
+
         Intent miIntent = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(miIntent);
     }
